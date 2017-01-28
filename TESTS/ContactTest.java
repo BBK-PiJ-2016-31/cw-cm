d7a29dba018a5130af3126c163291a52d11a7d2d
Damanjit <me@damanjit.com>
2017-01-28 23:48:17 +0000

    Project setup

---
+++ TESTS/ContactTest.java
@@ -0,0 +1,6 @@
+/**
+ * Created by D on 28/01/2017.
+ */
+public class ContactTest {
+
+}
---
+++ src/ContactImpl.java
@@ -0,0 +1,43 @@
+/**
+ * Created by D on 28/01/2017.
+ */
+public class ContactImpl implements Contact {
+    /**
+     * Returns the ID of the contact.
+     *
+     * @return the ID of the contact.
+     */
+    int getId(){
+
+    }
+
+    /**
+     * Returns the name of the contact.
+     *
+     * @return the name of the contact.
+     */
+    String getName(){
+
+    }
+
+    /**
+     * Returns our notes about the contact, if any.
+     *
+     * If we have not written anything about the contact, the empty
+     * string is returned.
+     *
+     * @return a string with notes about the contact, maybe empty.
+     */
+    String getNotes(){
+
+    }
+
+    /**
+     * Add notes about the contact.
+     *
+     * @param note the notes to be added
+     */
+    void addNotes(String note){
+
+    }
+}
