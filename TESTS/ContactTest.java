c5523f993f4035d7d0218cc000b3171c54640a45
Damanjit <me@damanjit.com>
2017-01-29 16:13:24 +0000

    Contact file working with contact test

--- TESTS/ContactTest.java
+++ TESTS/ContactTest.java
@@ -1,36 +1,45 @@
 /**
  * Created by D on 28/01/2017.
  */
-import org.junit.BeforeClass;
+
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ContactTest {
 
- //   @BeforeClass
+    Contact trial1;
+    Contact trial2;
 
-
+    @Before
+    public void setUp()
+    {
+        trial1 = new ContactImpl(2222, "John", "Boss");
+        trial2 = new ContactImpl(1111, "Mike");
+    }
 
     @Test
     public void nameTest(){
-        Contact trial1 = new ContactImpl(2222, "John", "Boss");
-        Contact trial2 = new ContactImpl(1111, "Mike");
         assertEquals("Names Don't Match", trial1.getName(),"John" );
         assertEquals("Names Don't Match", trial2.getName(),"Mike" );
     }
+
     @Test
     public void idTest(){
-        Contact trial1 = new ContactImpl(2222,"John","Boss");
-        Contact trial2 = new ContactImpl(1111,"Mike");
         assertEquals("IDs Don't Match", trial1.getId(), 2222 );
         assertEquals("IDs Don't Match", trial2.getId(),1111 );
     }
 
     @Test
     public void notesTest(){
-        Contact trial1 = new ContactImpl(2222, "John", "Boss");
-        Contact trial2 = new ContactImpl(1111, "Mike");
-        assertEquals("Names Don't Match", trial1.getNotes(),"Boss" );
-        assertEquals("Names Don't Match", trial2.getNotes(),null );
+        assertEquals("Notes1 Don't Match", trial1.getNotes(),"Boss" );
+        assertEquals("Notes2 Don't Match", trial2.getNotes(),null );
+
+        String str1= "This note is added to trial1";
+        String str2= "This note is added to trial2";
+        trial1.addNotes(str1);
+        trial2.addNotes(str2);
+        assertEquals("Notes1 Don't Match", "Boss"+str1,"Boss"+str1);
+        assertEquals("Notes2 Don't Match", str2,str2);
     }
 }
--- src/ContactImpl.java
+++ src/ContactImpl.java
@@ -1,10 +1,5 @@
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
-/**
- * Created by D on 28/01/2017.
- */
-
 public class ContactImpl implements Contact {
+
     private String name;
     private String notes;
     private int id;
@@ -19,49 +14,27 @@ public class ContactImpl implements Contact {
         this(id, name, null);
     }
 
-    /**
-     * Returns the ID of the contact.
-     *
-     * @return the ID of the contact.
-     */
     @Override
     public int getId(){
         //throw new NotImplementedException();
         return this.id;
     }
 
-    /**
-     * Returns the name of the contact.
-     *
-     * @return the name of the contact.
-     */
     @Override
     public String getName(){
         //throw new NotImplementedException();
         return this.name;
     }
 
-    /**
-     * Returns our notes about the contact, if any.
-     *
-     * If we have not written anything about the contact, the empty
-     * string is returned.
-     *
-     * @return a string with notes about the contact, maybe empty.
-     */
     @Override
     public String getNotes(){
         //throw new NotImplementedException();
         return this.notes;
     }
 
-    /**
-     * Add notes about the contact.
-     *
-     * @param note the notes to be added
-     */
     @Override
     public void addNotes(String note){
-        throw new NotImplementedException();
+        //throw new NotImplementedException();
+        this.notes+=note;
     }
 }
