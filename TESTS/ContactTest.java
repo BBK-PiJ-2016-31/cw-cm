c5db7f2a31e2a401995c4887a01a8f6638d05ba0
Damanjit <me@damanjit.com>
2017-01-29 00:01:58 +0000

    first test writing in progress

--- TESTS/ContactTest.java
+++ TESTS/ContactTest.java
@@ -1,6 +1,13 @@
 /**
  * Created by D on 28/01/2017.
  */
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class ContactTest {
 
+    @Test
+    public void getNameTest(){
+
+    }
 }
