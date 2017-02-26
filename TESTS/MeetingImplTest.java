b3c736d83d0caf521ae5d7373b96228d4959d746
Damanjit <me@damanjit.com>
2017-02-26 10:36:20 +0000

    Date comparison test complete.
    Used the Gregorian calendar objects and comparable class.

--- TESTS/MeetingImplTest.java
+++ TESTS/MeetingImplTest.java
@@ -3,23 +3,25 @@
  */
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class MeetingImplTest {
 
+		Calendar date;
+		Calendar date1;
 		@Test
 		public void setMeeting(){
-				Calendar date = new Calendar();
-				date.set(2017,03,01,09,30);
+				date = new GregorianCalendar();
+				date.set(2017,3,1,9,33);
 
-				Calendar date = new Calendar();
-				date.set(2017,03,01,09,30);
+				date1 = new GregorianCalendar();
+				date1.set(2017,3,1,9,33);
 
+				System.out.println(date.compareTo(date1));
 //				Set<Contact> contact
 //				Meeting meeting = new MeetingImpl(1234,date,contacts ) {
 				}
-
-		}
 }
