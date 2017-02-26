d34593629b1106910430bed7d35fa33da119e672
Damanjit <me@damanjit.com>
2017-02-26 13:00:18 +0000

    PrintContacts is not picking up previous meetings

--- TESTS/MeetingImplTest.java
+++ TESTS/MeetingImplTest.java
@@ -4,24 +4,42 @@
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class MeetingImplTest {
 
-		Calendar date;
-		Calendar date1;
+		Calendar date = new GregorianCalendar();
+		Calendar date1 = new GregorianCalendar();
+		Set<Contact> contacts = new HashSet<>();
+		Meeting meeting;
+
+		@Before
+		public void createData() {
+				date.set(2017,3,1,9,33);
+				date1.set(2017,3,1,9,33);
+				for (int i = 1; i < 10; i++) {
+						contacts.add(new ContactImpl(i,"A"+i, "Demo notes"));
+				}
+		}
+
 		@Test
 		public void setMeeting(){
-				date = new GregorianCalendar();
-				date.set(2017,3,1,9,33);
+				meeting = new FutureMeetingImpl(1234,date,this.contacts );
+		}
 
-				date1 = new GregorianCalendar();
-				date1.set(2017,3,1,9,33);
-
-				System.out.println(date.compareTo(date1));
-//				Set<Contact> contact
-//				Meeting meeting = new MeetingImpl(1234,date,contacts ) {
-				}
+		@Test
+		public void printContacts(){
+				contacts = meeting.getContacts();
+				System.out.println(contacts.isEmpty());
+				for (Contact contact:contacts)
+						System.out.println(
+								"Name: " + contact.getName() + " id: " + contact.getId() + " Notes: " + contact
+										.getNotes());
+		}
 }
