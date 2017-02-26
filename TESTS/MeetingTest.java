e3466a740556bda5f29d9c0189ef163b03fd6e3e
Damanjit <me@damanjit.com>
2017-02-26 10:19:49 +0000

    Testing the Date comparison

--- TESTS/MeetingTest.java
+++ TESTS/MeetingTest.java
@@ -2,10 +2,24 @@
  * Created by Damanjit on 26/02/2017.
  */
 
+import java.util.Calendar;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-public class MeetingTest {
+public class MeetingImplTest {
 
+		@Test
+		public void setMeeting(){
+				Calendar date = new Calendar();
+				date.set(2017,03,01,09,30);
+
+				Calendar date = new Calendar();
+				date.set(2017,03,01,09,30);
+
+//				Set<Contact> contact
+//				Meeting meeting = new MeetingImpl(1234,date,contacts ) {
+				}
+
+		}
 }
