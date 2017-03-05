f9a7ba96ac99ee701f40d5c9c48e5ffa2c070a61
Damanjit <me@damanjit.com>
2017-03-05 19:41:19 +0000

    Future meeting class with one constructor

--- src/FutureMeetingImpl.java
+++ src/FutureMeetingImpl.java
@@ -1,41 +1,17 @@
+/**
+ * Created by Damanjit on 26/02/2017.
+ */
+
 import java.util.Calendar;
 import java.util.Set;
 
-/**
- * A meeting to be held in the future
- * The class implementing this interface must have only one constructor
- * with three parameters: an ID (int), a date, and a set of contacts
- * If any of the references / pointers passed as parameters is
- * null, a NullPointerException must be thrown.
- */
-public class FutureMeetingImpl implements FutureMeeting {
+public class FutureMeetingImpl extends MeetingImpl implements FutureMeeting {
 
-    private int id;
-    private Calendar date;
-    private Set<Contact> contact;
-
-    /**
-    * Constructor to create a new future meeting®
-    */
-
+    /** Creates a new past meeting
+     * @throws IllegalArgumentException if the contact set is empty or a negative parameter ID
+     * @throws NullPointerException if the user sends null Date
+     */
     public FutureMeetingImpl(int id, Calendar date, Set<Contact> setContacts) {
-        this.id = id;
-        this.date = date;
-        this.contact = setContacts;
-    }
-
-    @Override
-    public int getId() {
-      return 0;
-    }
-
-    @Override
-    public Calendar getDate() {
-      return date;
-    }
-
-    @Override
-    public Set<Contact> getContacts() {
-      return this.contact;
+        super(id, date, setContacts);
     }
 }
