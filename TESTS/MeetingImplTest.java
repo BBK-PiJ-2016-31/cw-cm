e406cd32ff2c67d62f99265ac23afd60fec1b442
Damanjit <me@damanjit.com>
2017-03-05 12:44:01 +0000

    Method name changed to reflect its use

--- TESTS/MeetingImplTest.java
+++ TESTS/MeetingImplTest.java
@@ -46,4 +46,44 @@ public class MeetingImplTest {
 						i++;
 				}
 		}
+
+		@Test
+		public void corruptingData() {
+				date.set(2017,3,1,9,33);
+				date1.set(2017,3,1,9,33);
+				for (int i = 1; i < 10; i++) {
+						contacts.add(new ContactImpl(i,"A"+i, "Demo notes"));
+				}
+				try{
+						System.out.print("1) \t\tInserting null Date - \t\t\t");
+						meeting = new MeetingImplMock(1234,null,this.contacts );
+				} catch (NullPointerException e){
+						System.out.println("1) NullPointerException generated");
+				}
+				try{
+						System.out.print("2) \t\tInserting null Contacts - \t");
+						meeting = new MeetingImplMock(1234,date,null );
+				} catch (IllegalArgumentException e){
+						System.out.println("2) IllegalArgumentException Generated");
+				}
+				try{
+						System.out.print("2.1)\tInserting Empty Contacts - \t");
+						meeting = new MeetingImplMock(1234,date,new LinkedHashSet<Contact>() );
+				} catch (IllegalArgumentException e){
+						System.out.println("2.1) IllegalArgumentException Generated");
+				}
+				try{
+						System.out.print("3) \t\tInserting -Ve ID - \t\t\t\t\t");
+						meeting = new MeetingImplMock(-1234,date,this.contacts );
+				} catch (IllegalArgumentException e){
+						System.out.println("3) IllegalArgumentException Generated");
+				}
+				try{
+						System.out.print("4) \t\tInserting ZERO ID - \t\t\t\t");
+						meeting = new MeetingImplMock(0,date,this.contacts );
+				} catch (IllegalArgumentException e){
+						System.out.println("4) IllegalArgumentException Generated");
+				}
+		}
+
 }
