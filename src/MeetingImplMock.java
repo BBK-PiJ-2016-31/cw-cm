50124327b9369af776da05cf3552793164e0e4f9
Damanjit <me@damanjit.com>
2017-03-05 11:47:21 +0000

    New class to test the Abstract MeetingImpl class

---
+++ src/MeetingImplMock.java
@@ -0,0 +1,23 @@
+import java.util.Calendar;
+import java.util.Set;
+
+/**
+ * Created by Damanjit on 05/03/2017.
+ */
+public class MeetingImplMock extends MeetingImpl {
+
+		/**
+		 * Creates a meeting.
+		 * Main constructor to create a meeting object.
+		 *
+		 * @param id - Id of the meeting object
+		 * @param date - Date of the meeting
+		 * @param contacts - Set of contacts which will take part in the meeting
+		 * @throws IllegalArgumentException if the contact set is empty or the date is less than or equal to
+		 * zero
+		 * @throws NullPointerException if the user sends null Date
+		 */
+		public MeetingImplMock(int id, Calendar date, Set<Contact> contacts) {
+				super(id, date, contacts);
+		}
+}
