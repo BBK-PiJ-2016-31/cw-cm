549ed9346d07cad9c845f80900cdab6329e5a868
Damanjit <me@damanjit.com>
2017-03-05 11:17:21 +0000

    Improved comments. More comments added.

--- src/MeetingImpl.java
+++ src/MeetingImpl.java
@@ -10,8 +10,12 @@ public abstract class MeetingImpl implements Meeting {
     private Set<Contact> contacts;
 
     /** Creates a meeting.
-     * @throws IllegalArgumentException if the contact set is empty
-     * @throws NullPointerException if the user sends null Date or a negative parameter ID
+     * Main constructor to create a meeting object.
+     * @param id  - Id of the meeting object
+     * @param date - Date of the meeting
+     * @param contacts  - Set of contacts which will take part in the meeting
+     * @throws IllegalArgumentException if the contact set is empty or the date is less than or equal to zero
+     * @throws NullPointerException if the user sends null Date
      */
     public MeetingImpl(int id, Calendar date, Set<Contact> contacts) {
         if (contacts.isEmpty() || id<=0) {
