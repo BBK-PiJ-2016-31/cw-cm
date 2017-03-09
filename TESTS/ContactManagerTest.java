43c8c3e24e31d5442804b42b3cc8d19e95c3fb65
Damanjit <me@damanjit.com>
2017-03-09 16:04:00 +0000

    addContact to be tested first

--- TESTS/ContactManagerTest.java
+++ TESTS/ContactManagerTest.java
@@ -11,23 +11,37 @@ import static org.junit.Assert.*;
 
 public class ContactManagerTest {
 
-		private Set<Contact> contacts = new LinkedHashSet<>();
-		private Calendar date = new GregorianCalendar();
-		private ContactManager manager = new ContactManagerImpl();
+	ContactManager manager = new ContactManagerImpl();
 
-		@Before
-		public void SetUp(){
-				date.set(2017,3,1,9,33);
-				for (int i = 1; i < 10; i++) {
-						contacts.add(new ContactImpl(i,"A"+i, "Demo notes"));
-				}
+	@Test
+	public void newContactTest(){
+		manager.addNewContact("A","noteA");
+		manager.addNewContact("B","noteB");
+		manager.addNewContact("C","noteC");
+		boolean exception = false;
+		try{
+			manager.addNewContact("D","");
+		} catch (IllegalArgumentException e){
+			exception=true;
 		}
+		assertTrue("Empty Notes - Exception expected", exception);
 
-		@Test
-		public void futureMeetingSetGet(){
-				int id = manager.addFutureMeeting(contacts, date);
-				Meeting object = manager.getFutureMeeting(id);
-				assertEquals("Meeting IDs don't match", object.getId(),id);
+		exception = false;
+		try{
+			manager.addNewContact("","Notes");
+		} catch (IllegalArgumentException e){
+			exception=true;
 		}
+		assertTrue("Empty Name - Exception expected", exception);
+
+		exception = false;
+		try{
+			manager.addNewContact("D", null);
+		} catch (IllegalArgumentException e){
+			exception=true;
+		}
+		assertTrue("null Date - Exception expected", exception);
+
+	}
 
 }
