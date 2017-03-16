85e6ffbe37c5a168cc9791507500ad68d22a9cd8
Damanjit <me@damanjit.com>
2017-03-16 11:11:42 +0000

    Indentation error sorted

--- src/main/java/impl/MeetingImplMock.java
+++ src/main/java/impl/MeetingImplMock.java
@@ -9,18 +9,18 @@ import spec.Contact;
  */
 public class MeetingImplMock extends MeetingImpl {
 
-		/**
-		 * Creates a meeting.
-		 * Main constructor to create a meeting object.
-		 *
-		 * @param id - Id of the meeting object
-		 * @param date - Date of the meeting
-		 * @param contacts - Set of contacts which will take part in the meeting
-		 * @throws IllegalArgumentException if the contact set is empty or the date is less than or equal to
-		 * zero
-		 * @throws NullPointerException if the user sends null Date
-		 */
-		public MeetingImplMock(int id, Calendar date, Set<Contact> contacts) {
-				super(id, date, contacts);
-		}
+ /**
+  * Creates a meeting.
+  * Main constructor to create a meeting object.
+  *
+  * @param id - Id of the meeting object
+  * @param date - Date of the meeting
+  * @param contacts - Set of contacts which will take part in the meeting
+  * @throws IllegalArgumentException if the contact set is empty or the date is less than or equal to
+  * zero
+  * @throws NullPointerException if the user sends null Date
+  */
+	public MeetingImplMock(int id, Calendar date, Set<Contact> contacts) {
+		super(id, date, contacts);
+	}
 }
